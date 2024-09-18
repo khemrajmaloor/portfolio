2b2915087b347118d55459f03258afc310cf5500
Khemraj <khemraj@rwit.io>
2024-09-18 15:49:01 +0530

    Include meter js file in this file

--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -5,7 +5,7 @@
      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
      <title>Document</title>
      <link rel="stylesheet" href="font.css" />
-     @vite('resources/css/app.css')
+     @vite(['resources/css/app.css', 'resources/js/app.js'])
      <script src="./matter.js" defer></script>
  </head>
  <body class="bg-[#111] text-white font-['Nunito']">
