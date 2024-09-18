eddb15ade4fa605f6bfc2923d88ea4ad2002173e
Khemraj <khemraj@rwit.io>
2024-09-18 15:28:21 +0530

    Create home page here

--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   @vite('resources/css/app.css')
-  <title>Document</title>
-</head>
-<body>
-  <h1>Header</h1>
-  
+ <html lang="en">
+
+ <head>
+     <meta charset="UTF-8" />
+     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+     <title>Document</title>
+     <link rel="stylesheet" href="font.css" />
+     @vite('resources/css/app.css')
+     <script src="./matter.js" defer></script>
+ </head>
+ <body class="bg-[#111] text-white font-['Nunito']">
