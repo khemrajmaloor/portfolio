159b23ccdadc2d9b0999dd55a2ac26d1ba889a6b
Khemraj <khemraj@rwit.io>
2024-09-19 11:11:04 +0530

    added  html structure code

--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -5,6 +5,7 @@
      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
      <title>Document</title>
      <link rel="stylesheet" href="font.css" />
+     <link rel="stylesheet"  href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
      @vite(['resources/css/app.css', 'resources/js/app.js'])
      <script src="./matter.js" defer></script>
  </head>
